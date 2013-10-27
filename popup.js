cc74d547d2e1cfca65307ee1714ca615b09df062
Kelly Reynolds <kelly@lawrence.(none)>
2013-10-27 16:18:53 -0400

    initial

---
+++ popup.js
@@ -0,0 +1,7 @@
+// This is the script to grab the URL and pass it to the scraper for ShopGodt
+void((function(d){var e=d.createElement('script');
+e.setAttribute('type','text/javascript');
+e.setAttribute('charset','UTF-8');
+e.setAttribute('src','verify.js');
+e.setAttribute('id','shopgodtjs');
+d.body.appendChild(e)})(document));
